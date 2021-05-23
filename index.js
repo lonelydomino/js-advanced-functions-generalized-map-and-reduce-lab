18d5155ab95b666a7137a82d6a870a0b9fd20eda
Michael Martinez <zeroeclipse@live.com>
2021-05-23 01:00:22 -0400

    commit tree 2db85c15774c72979cc1f82ad3be80390c96e979

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your functions here
+
+
+function map(array, callback) {
+    for (let i = 0; i < array.length; i++) {
+        array[i] = callback(array[i]);
+      }
+    return array
+}
+function reduce(array, callback, start) {
+    let total
+    let i
+    if (start) {
+      total = start
+      i = 0
+    } else {
+      total = array[0]
+      i = 1
+    }
+    for (i; i < array.length; i++) {
+      total = callback(array[i], total);
+    }
+    return total;
+}
